fa687c7cb553782cc49e2921b805eeca04843d49
IWANABETHATGUY <974153916@qq.com>
2020-07-05 21:32:14 +0800

    feat: 🎸 add www category

---
+++ www/webpack.config.js
@@ -0,0 +1,14 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    filename: "index.js",
+  },
+  mode: "production",
+  plugins: [
+    new CopyWebpackPlugin(['index.html'])
+  ],
+};
